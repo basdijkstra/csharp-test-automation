9ea9236cafff1190ee6ce3eee0ec387cf760614b
Bas Dijkstra <bas@ontestautomation.com>
2020-02-13 14:18:59 +0100

    Added preparation test

---
+++ Preparation/PreparationTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System.Net;
+
+namespace Preparation
+{
+    [TestClass]
+    public class PreparationTest
+    {
+        [TestMethod]
+        public void InvokeZippopotamUs_CheckStatusCode_ShouldBeHttp200Ok()
+        {
+            // Arrange
+            RestClient client = new RestClient("http://api.zippopotam.us");
+            RestRequest request = new RestRequest("/us/90210", Method.GET);
+
+            // Act
+            IRestResponse response = client.Execute(request);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
